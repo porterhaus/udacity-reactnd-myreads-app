009d5974556a6bfeaf0bf232783bd523a84d6047
porterhaus <bporter@porterhaus.com>
2017-12-13 18:01:45 -0600

    refactor: Update BookList to stateless functional component

--- src/components/BookList.js
+++ src/components/BookList.js
@@ -3,48 +3,47 @@ import { Link } from 'react-router-dom';
 import PropTypes from 'prop-types';
 import BookShelf from './BookShelf';
 
-class BookList extends React.Component {
-  filterBooks = (shelf) => {
-    return this.props.listBooks.filter(b => b.shelf === shelf);
+const BookList = ({listBooks, changeBookShelf}) => {
+  const filterBooks = (shelf) => {
+    return listBooks.filter(b => b.shelf === shelf)
   };
-
-  render () {
-    return (
-      <div>
-        <div className="list-books"> 
-          <div className="list-books-title">
-            <h1>MyReads</h1>
-          </div>
-          <div className="list-books-content">
-            <div>
-              <BookShelf
-                title={'Currently Reading'}
-                books={this.filterBooks('currentlyReading')}
-                changeBookShelf={this.props.changeBookShelf}
-              />
-              <BookShelf
-                title={'Want to Read'}
-                books={this.filterBooks('wantToRead')}
-                changeBookShelf={this.props.changeBookShelf}
-              />
-              <BookShelf
-                title={'Read'}
-                books={this.filterBooks('read')}
-                changeBookShelf={this.props.changeBookShelf}
-              />
-            </div>
-          </div>
+  
+  return (
+    <div>
+      <div className="list-books"> 
+        <div className="list-books-title">
+          <h1>MyReads</h1>
         </div>
-        <div className="open-search">
-            <Link to='/search'>Add a book</Link>
+        <div className="list-books-content">
+          <div>
+            <BookShelf
+              title={'Currently Reading'}
+              books={filterBooks('currentlyReading')}
+              changeBookShelf={changeBookShelf}
+            />
+            <BookShelf
+              title={'Want to Read'}
+              books={filterBooks('wantToRead')}
+              changeBookShelf={changeBookShelf}
+            />
+            <BookShelf
+              title={'Read'}
+              books={filterBooks('read')}
+              changeBookShelf={changeBookShelf}
+            />
+          </div>
         </div>
       </div>
-    );
-  }
-}
+      <div className="open-search">
+          <Link to='/search'>Add a book</Link>
+      </div>
+    </div>
+  );
+};
 
 BookList.propTypes = {
-  listBooks: PropTypes.array.isRequired
+  listBooks: PropTypes.array.isRequired,
+  changeBookShelf: PropTypes.func.isRequired
 };
 
 export default BookList;
